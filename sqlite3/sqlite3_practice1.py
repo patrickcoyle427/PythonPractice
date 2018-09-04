015616dc2f64189ac3f01bd6b012d420912ad7e9
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2018-09-04 17:41:58 -0400

    Add files via upload

---
+++ sqlite3/sqlite3_practice1.py
@@ -0,0 +1,38 @@
+# Practice obtained from:
+# https://www.python-course.eu/sql_python.php
+
+import sqlite3
+
+connection = sqlite3.connect('company.db')
+
+# delete the current employee table.
+# cursor.execute("""DROP TABLE employee;""")
+# Use this or delete the company.db file to run this script
+
+cursor = connection.cursor()
+
+sql_command = '''
+CREATE TABLE employee(
+staff_number INTEGER PRIMARY KEY,
+fname VARCHAR(20),
+lname VARCHAR(30),
+gender CHAR(1),
+joining DATE,
+birth_date DATE);'''
+
+cursor.execute(sql_command)
+
+sql_command = '''INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
+                 VALUES (NULL, "William", "Shakespeare", "m", "1961-10-25");'''
+
+cursor.execute(sql_command)
+
+sql_command = '''INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
+              VALUES (NULL, "Frank", "Schiller", "m", "1955-08-17");'''
+
+cursor.execute(sql_command)
+
+#never forget this, if you want the changes to be saved:
+connection.commit()
+connection.close()
+
